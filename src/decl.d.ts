ad4bc62297f378497e7907959fc6651f4c16eaf7
WriteCoin <sovtsardom@gmail.com>
2021-09-16 22:48:59 +0300

    добавлен тип order

--- src/decl.d.ts
+++ src/decl.d.ts
@@ -2,6 +2,7 @@ declare type integer = number
 declare type real = number
 declare type Primitive = boolean | integer | real | string
 declare type rawcode = integer | string
+declare type order = rawcode
 
 declare type code = () => void
 declare type codeboolexpr = () => boolean
